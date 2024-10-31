2c48f444bd7b6d8a0762eb35b97265adff884396
bahrinanwar <heibahrin@gmail.com>
2024-10-31 17:37:40 +0700

    commit

---
+++ app/src/main/java/com/bahrin/projectakhir_bahrin/MainActivity.java
@@ -0,0 +1,14 @@
+package com.bahrin.projectakhir_bahrin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
